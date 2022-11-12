702216601646d8b63438e9e64964c0714e722807
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-12 00:30:25 -0800

    Updated Index with React.StrictMode

--- odyssey-lift-off-part2/client/src/index.js
+++ odyssey-lift-off-part2/client/src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
-});
+})
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
+  <React.StrictMode>
+    <ApolloProvider client={ client }>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
