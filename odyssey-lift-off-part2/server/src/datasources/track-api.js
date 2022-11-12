7b04c2083018a762db757e0ce55f49b04089f116
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-12 00:29:18 -0800

    Updated TrackAPI  Data Source

--- odyssey-lift-off-part2/server/src/datasources/track-api.js
+++ odyssey-lift-off-part2/server/src/datasources/track-api.js
@@ -7,7 +7,7 @@ class TrackAPI extends RESTDataSource {
     }
 
     getTracksForHome(){
-        return this.getTracksForHome('tracks')
+        return this.get('tracks')
     }
 
     getAuthor(authorID) {
@@ -15,4 +15,4 @@ class TrackAPI extends RESTDataSource {
     }
 }
 
-module.exports = TrackAPI
+module.exports = TrackAPI;
