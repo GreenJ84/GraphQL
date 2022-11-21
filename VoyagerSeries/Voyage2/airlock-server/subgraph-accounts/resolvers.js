fcffac3fc5de4d62fdc64017982997f4e0de2d24
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-21 14:56:29 -0800

    Added reference resolvers for accounts subgraph

--- VoyagerSeries/Voyage2/airlock-server/subgraph-accounts/resolvers.js
+++ VoyagerSeries/Voyage2/airlock-server/subgraph-accounts/resolvers.js
@@ -1,8 +1,20 @@
 const { AuthenticationError, ForbiddenError } = require('./utils/errors');
 
 const resolvers = {
-  Query: {
-    example: () => 'Hello World!',
+  User: {
+    __resolveType(user) {
+      return user.role;
+    },
+  },
+  Host: {
+    __resolveReference: (user, { dataSources }) => {
+      return dataSources.accountsAPI.getUser(user.id)
+    }
+  },
+  Guest: {
+    __resolveReference: (user, { dataSources }) => {
+      return dataSources.accountsAPI.getUser(user.id)
+    }
   },
 };
 
