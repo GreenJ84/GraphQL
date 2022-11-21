44dae7d3e4b854d888e793d81bfe1328a890e361
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-21 15:06:36 -0800

    Connected accounts dataSource

--- VoyagerSeries/Voyage2/airlock-server/subgraph-accounts/index.js
+++ VoyagerSeries/Voyage2/airlock-server/subgraph-accounts/index.js
@@ -1,15 +1,16 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const { buildSubgraphSchema } = require('@apollo/subgraph');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
 
-const { readFileSync } = require('fs');
-const axios = require('axios');
-const gql = require('graphql-tag');
+const { readFileSync } = require("fs");
+const axios = require("axios");
+const gql = require("graphql-tag");
 
-const { AuthenticationError } = require('./utils/errors');
+const { AuthenticationError } = require("./utils/errors");
 
-const typeDefs = gql(readFileSync('./schema.graphql', { encoding: 'utf-8' }));
-const resolvers = require('./resolvers');
+const typeDefs = gql(readFileSync("./schema.graphql", { encoding: "utf-8" }));
+const resolvers = require("./resolvers");
+const AccountsAPI = require("./datasources/accounts");
 
 async function startApolloServer() {
   const server = new ApolloServer({
@@ -19,20 +20,22 @@ async function startApolloServer() {
     }),
   });
 
-  const port =4002;
-  const subgraphName = 'accounts';
-  
+  const port = 4002;
+  const subgraphName = "accounts";
+
   try {
     const { url } = await startStandaloneServer(server, {
       context: async ({ req }) => {
-        const token = req.headers.authorization || '';
-        const userId = token.split(' ')[1]; // get the user name after 'Bearer '
+        const token = req.headers.authorization || "";
+        const userId = token.split(" ")[1]; // get the user name after 'Bearer '
 
         let userInfo = {};
         if (userId) {
-          const { data } = await axios.get(`http://localhost:4011/login/${userId}`).catch((error) => {
-            throw AuthenticationError();
-          });
+          const { data } = await axios
+            .get(`http://localhost:4011/login/${userId}`)
+            .catch((error) => {
+              throw AuthenticationError();
+            });
 
           userInfo = { userId: data.id, userRole: data.role };
         }
@@ -40,7 +43,7 @@ async function startApolloServer() {
         return {
           ...userInfo,
           dataSources: {
-            // TODO: add data sources here
+            accountsAPI: new AccountsAPI(),
           },
         };
       },
